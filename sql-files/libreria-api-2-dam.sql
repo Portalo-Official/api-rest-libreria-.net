389ceb9c7918ee6affd47a5d863b2ee36f21e823
santiagoieshna <smiguezc01@iesarroyoharnina.es>
2024-02-17 14:10:41 +0100

    fix sql-file

--- sql-files/libreria-api-2-dam.sql
+++ sql-files/libreria-api-2-dam.sql
@@ -209,13 +209,16 @@ DELIMITER ;
 -- ########################### CREAR USUARIO ####################
 -- CREATE USER 'portalo'@'%' -> PAra conectarse desde cualqueir host
 
-/* CREATE USER 'portalo'@'localhost' IDENTIFIED BY '12345';
+CREATE USER 'portalo'@'localhost' IDENTIFIED BY '12345';
 GRANT ALL PRIVILEGES ON LibreriaNET.* TO 'portalo'@'localhost';
-
+GRANT ALL PRIVILEGES ON LibreriaNET.* TO 'portalo'@'%';
+--GRANT ALL PRIVILEGES ON *.* TO 'portalo'@'192.168.1.%';
+--GRANT ALL PRIVILEGES ON *.* TO 'portalo'@'localhost';
 -- Refrescar privilegios
 FLUSH PRIVILEGES; 
 
 -- Dara error el conectarnos por el tema host
+/*
 GRANT ALL PRIVILEGES ON *.* TO 'portalo'@'192.168.1.%' 
 IDENTIFIED BY '12345' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
@@ -274,7 +277,7 @@ CREATE TABLE Libros(
 	id int(9) PRIMARY KEY AUTO_INCREMENT,
 	titulo varchar(250) not null,
 	isbn varchar(13) not null UNIQUE,
-	precio decimal(5,2) not null,
+	precio decimal(7,2) not null,
 	id_autor int(9) not null,
 	id_tema int(9) not null,
 	id_edicion int(9) not null,
@@ -306,10 +309,12 @@ CREATE OR REPLACE PROCEDURE getLibros()
 			L.isbn      as ISBN,
 			L.titulo    as Titulo,
 			A.nombre    as Autor,
-			T.tema      as Tipo,
+			T.tema      as Tema,
 			E.tipo      as Edicion,
 			F.tipo      as Formato,
-			A2.cantidad as Stock
+			L.Precio    as Precio,
+			A2.cantidad as Stock,
+			L.URL      	as URL
 		FROM LIBROS L
 		INNER JOIN temas     T  ON L.id_tema    = T.id
 		INNER JOIN autores   A  ON L.id_autor   = A.id
@@ -321,17 +326,19 @@ DELIMITER ;
 
 -- GET libroBYID
 DELIMITER $$
-CREATE OR REPLACE PROCEDURE getLibrosByISBN(IN p_isbn varchar(13))
+CREATE OR REPLACE PROCEDURE getLibroByISBN(IN p_isbn varchar(13))
 	BEGIN
 		SELECT 
 			L.id        as Id,
 			L.isbn      as ISBN,
 			L.titulo    as Titulo,
 			A.nombre    as Autor,
-			T.tema      as Tipo,
+			T.tema      as Tema,
 			E.tipo      as Edicion,
 			F.tipo      as Formato,
-			A2.cantidad as Stock
+			L.Precio    as Precio,
+			A2.cantidad as Stock,
+			L.URL       as URL
 		FROM LIBROS L
 		INNER JOIN temas     T  ON L.id_tema    = T.id
 		INNER JOIN autores   A  ON L.id_autor   = A.id
@@ -351,7 +358,8 @@ CREATE OR REPLACE PROCEDURE createLibro(IN p_isbn    VARCHAR(13),
 										IN p_edicion varchar(25),
 										IN p_tema    varchar(50),
 										IN p_formato varchar(25),
-										IN p_stock   int(9)
+										IN p_stock   int(9),
+										IN p_url     VARCHAR(1000)
 										)
 	BEGIN
 		Declare v_id_autor INT;
@@ -364,8 +372,8 @@ CREATE OR REPLACE PROCEDURE createLibro(IN p_isbn    VARCHAR(13),
 		SELECT id INTO v_id_tema    FROM temas     WHERE tema   = p_tema    limit 1;
 		SELECT id INTO v_id_formato FROM formatos  WHERE tipo   = p_formato limit 1;
 		-- Crear libro
-		INSERT INTO Libros(titulo, isbn, precio, id_autor, id_tema, id_edicion, id_formato)
-		VALUES(p_titulo, p_isbn, p_precio, v_id_autor, v_id_tema, v_id_edicion, v_id_formato);
+		INSERT INTO Libros(titulo, isbn, precio, id_autor, id_tema, id_edicion, id_formato, URL)
+		VALUES(p_titulo, p_isbn, p_precio, v_id_autor, v_id_tema, v_id_edicion, v_id_formato, p_URL);
 		-- Crear registro almacen
 		INSERT INTO almacen(libro,cantidad) VALUES (p_isbn, p_stock);
 	END$$
@@ -383,14 +391,14 @@ CREATE OR REPLACE PROCEDURE updateLibro(IN p_isbn    VARCHAR(13),
 										)
 	BEGIN
 		Declare v_id_autor INT;
+		-- Buscar IDs , p_autor -> p de parametro
 		Declare v_id_edicion INT;
 		Declare v_id_tema INT;
 		Declare v_id_formato INT;
-		-- Buscar IDs , p_autor -> p de parametro
 		SELECT id INTO v_id_autor   FROM autores   WHERE nombre = p_autor   limit 1;
 		SELECT id INTO v_id_edicion FROM ediciones WHERE tipo   = p_edicion limit 1;
 		SELECT id INTO v_id_tema    FROM temas     WHERE tema   = p_tema    limit 1;
-		SELECT id INTO v_id_formato FROM formatos  WHERE formato= p_formato limit 1;
+		SELECT id INTO v_id_formato FROM formatos  WHERE tipo= p_formato limit 1;
 		-- Actualizar
 		UPDATE Libros
    		SET
@@ -422,6 +430,7 @@ CREATE OR REPLACE PROCEDURE compraVenta(IN cantidadLibro int(9),
 	END $$ 
 DELIMITER ;
 
+/*
 CREATE TABLE USER(
 
 	id int(9) PRIMARY KEY AUTO_INCREMENT,
@@ -444,8 +453,9 @@ CREATE TABLE USER(
 	isbn varchar(25),
 	fechaVenta DATE,
 	precio decimal(6,2),
-)
+); */
 
+/*
 DELIMITER $$
 CREATE OR REPLACE TRIGGER before_update_almacen
 	BEFORE UPDATE ON ALMACEN
@@ -456,6 +466,7 @@ CREATE OR REPLACE TRIGGER before_update_almacen
 		-- Compra
 		Insert
 DELIMITER $$
+*/
 
 
 -- Poblar BBDD
@@ -467,7 +478,6 @@ call createTema("Fantasia");
 call createTema("Terror");
 call createTema("Romance");
 call createTema("Aventura");
-CALL createTema('Fantasia');
 CALL createTema('Ciencia Ficcion');
 CALL createTema('Misterio');
 CALL createTema('Historico');
@@ -508,18 +518,30 @@ INSERT INTO Autores(nombre) VALUES('Caballero Chavero')
 
 
 -- Generar libros
-CALL createLibro('9780007117116', 'Cien Años de Soledad', 24.99, 'Gabriel Garcia Marquez', 'Especial', 'Fantasia', 'Tapa Dura', 100);
-CALL createLibro('9780307352149', '1Q84', 21.99, 'Haruki Murakami', 'Aniversario', 'Ciencia Ficcion', 'Tapa Blanda', 80);
-CALL createLibro('9788498389327', 'Harry Potter y la Piedra Filosofal', 18.99, 'J.K. Rowling', 'Especial', 'Misterio', 'Digital', 120);
-CALL createLibro('9780553103540', 'Juego de Tronos', 29.99, 'George R.R. Martin', 'Aniversario', 'Historico', 'Tapa Dura', 90);
-CALL createLibro('9788408190013', 'Eva Luna', 27.99, 'Isabel Allende', 'Especial', 'Fantasia', 'Tapa Blanda', 110);
-CALL createLibro('9780061122415', 'El Alquimista', 25.99, 'Paulo Coelho', 'Aniversario', 'Ciencia Ficcion', 'Tapa Dura', 70);
-CALL createLibro('9780062693662', 'Asesinato en el Orient Express', 19.99, 'Agatha Christie', 'Especial', 'Misterio', 'Tapa Blanda', 95);
-CALL createLibro('9780307743657', 'It', 26.99, 'Stephen King', 'Aniversario', 'Historico', 'De bolsillo', 85);
-CALL createLibro('9780141439563', 'Orgullo y Prejuicio', 23.99, 'Jane Austen', 'Especial', 'Fantasia', 'Digital', 75);
-CALL createLibro('9780140449976', 'Los Miserables', 15.99, 'Victor Hugo', 'Aniversario', 'Ciencia Ficcion', 'Tapa Dura', 105);
-CALL createLibro('9780307474278', 'Crónica del pájaro que da cuerda al mundo', 20.99, 'Haruki Murakami', 'Especial', 'Misterio', 'Tapa Blanda', 65);
-CALL createLibro('9788490628588', 'Harry Potter y las Reliquias de la Muerte', 22.99, 'J.K. Rowling', 'Aniversario', 'Fantasía', 'De bolsillo', 88);
-CALL createLibro('9788497593464', 'El nombre del viento', 24.99, 'Jane Austen', 'Especial', 'Historico', 'Tapa Dura', 92);
-CALL createLibro('9780061122415', 'El Silmarillion', 28.99, 'J.K. Rowling', 'Aniversario', 'Ciencia Ficcion', 'Tapa Blanda', 78);
-CALL createLibro('9780307474292', 'Cazadores de sombras: Ciudad de Hueso', 21.99, 'Cassandra Clare', 'Especial', 'Fantasía', 'Tapa Dura', 102);
+CALL createLibro('9780007117116', 'Cien Años de Soledad', 24.99, 'Gabriel Garcia Marquez', 'Especial', 'Fantasia', 'Tapa Dura', 100, 'https://i.ibb.co/s9GR95M/images.jpg');
+CALL createLibro('9780307352149', '1Q84', 21.99, 'Haruki Murakami', 'Aniversario', 'Ciencia Ficcion', 'Tapa Blanda', 80, 'https://i.ibb.co/tZjjw69/1q84-libros-1-y-2-9788483836262.webp');
+CALL createLibro('9788498389327', 'Harry Potter y la Piedra Filosofal', 18.99, 'J.K. Rowling', 'Especial', 'Misterio', 'Digital', 120, 'https://i.ibb.co/3pmnqr1/51ux-Z1-Ek-T4-L.jpg');
+CALL createLibro('9780553103540', 'Juego de Tronos', 29.99, 'George R.R. Martin', 'Aniversario', 'Historico', 'Tapa Dura', 90,'https://i.ibb.co/z4TcJFm/Juego-de-tronos.jpg');
+CALL createLibro('9788408190013', 'Eva Luna', 27.99, 'Isabel Allende', 'Especial', 'Fantasia', 'Tapa Blanda', 110, 'https://i.ibb.co/jH1nKkB/visd-0000-JPG01-BVP.jpg');
+CALL createLibro('9780061122415', 'El Alquimista', 25.99, 'Paulo Coelho', 'Aniversario', 'Ciencia Ficcion', 'Tapa Dura', 70, 'https://i.ibb.co/GCcSHKL/portada-el-alquimista-paulo-coelho-201612191218.webp');
+CALL createLibro('9780062693662', 'Asesinato en el Orient Express', 19.99, 'Agatha Christie', 'Especial', 'Misterio', 'Tapa Blanda', 95, 'https://i.ibb.co/KX9LqLF/images.jpg');
+CALL createLibro('9780307743657', 'It', 26.99, 'Stephen King', 'Aniversario', 'Historico', 'De bolsillo', 85, 'https://i.ibb.co/G2d7GLZ/It-1986-front-cover-first-edition-1.jpg');
+CALL createLibro('9780141439563', 'Orgullo y Prejuicio', 23.99, 'Jane Austen', 'Especial', 'Fantasia', 'Digital', 75, 'https://i.ibb.co/48xBZb4/9788467070835.jpg');
+CALL createLibro('9780140449976', 'Los Miserables', 15.99, 'Victor Hugo', 'Aniversario', 'Ciencia Ficcion', 'Tapa Dura', 105,'https://i.ibb.co/xG8r92K/img-54-0-big.jpg');
+CALL createLibro('9780307474278', 'Crónica del pájaro que da cuerda al mundo', 20.99, 'Haruki Murakami', 'Especial', 'Misterio', 'Tapa Blanda', 65, 'https://i.ibb.co/Pmrv9jq/portada-cronica-del-pajaro-que-da-cuerda-al-mundo-haruki-murakami-201609281234.webp');
+CALL createLibro('9788490628588', 'Harry Potter y las Reliquias de la Muerte', 22.99, 'J.K. Rowling', 'Aniversario', 'Fantasía', 'De bolsillo', 88, 'https://i.ibb.co/ZxSJTq9/514rkihbko-L.jpg');
+CALL createLibro('9758410628688', 'Harry Potter y el prisionero de Azkaban', 29.79, 'J.K. Rowling', 'Especial', 'Fantasía', 'Tapa Dura', 40, 'https://i.ibb.co/ScjcV4x/51-g28v-UNDL.jpg');
+CALL createLibro('9788011593164', 'El nombre del viento', 24.99, 'Jane Austen', 'Especial', 'Historico', 'Tapa Dura', 92, 'https://i.ibb.co/HNYpGxk/978840133720.jpg');
+CALL createLibro('9780491272355', 'El Silmarillion', 28.99, 'J.K. Rowling', 'Aniversario', 'Ciencia Ficcion', 'Tapa Blanda', 78, 'https://i.ibb.co/m4J43Vk/81-Z7-OWa-Bs-ML-AC-UF1000-1000-QL80.jpg');
+CALL createLibro('9780503474192', 'Cazadores de sombras: Ciudad de Hueso', 21.99, 'Cassandra Clare', 'Especial', 'Fantasía', 'Tapa Dura', 102, 'https://i.ibb.co/h1gZryV/51-I7qd-Op6-GL-AC-UF1000-1000-QL80.jpg');
+CALL createLibro('9760007774242', 'Cazadores de sombras: Ciudad de las almas perdidas', 21.99, 'Cassandra Clare', 'Especial', 'Fantasía', 'Tapa Dura', 4, 'https://i.ibb.co/gDRwr2C/visd-0000-JPG019-T0.jpg');
+CALL createLibro('9710807117511', 'Del Amor a otros Demonios', 49.99, 'Gabriel Garcia Marquez', 'Especial', 'Fantasia', 'De bolsillo', 10, 'https://i.ibb.co/ZSyZ6cy/978849759242.jpg');
+CALL createLibro('9760807237519', 'Libro Prueba Prototipo', 129.99, 'Beltran Labios Bonitos', 'Especial', 'Fantasia', 'De bolsillo', 10, 'https://i.ibb.co/mz6F1Kp/167219770-127523866011194-8434310988880472619-n.jpg');
+
+-- Editar Libro
+CALL updateLibro('9710007117223', 'updatePrueba', 121.38, 'Beltran Labios Bonitos', 'Especial', 'Fantasia', 'Digital');
+
+/*
+URL para libros inventados
+https://i.ibb.co/mz6F1Kp/167219770-127523866011194-8434310988880472619-n.jpg
+*/
